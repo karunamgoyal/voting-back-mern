8ab0ea4b2bed1e91f0b44619da3258c65917a86a
karunam goyal <karunam.goyal25@gmail.com>
2019-06-15 11:48:54 +0530

    Database schema added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+//require('dotenv').config();
 const express = require('express');
-
-const handle = require('./handlers')
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./models');
+const handle = require('./handlers');
+// server express app init
 const app = express();
 
-const port = 4000;
+const port = 4000;//process.env.PORT || 4000;
 
 app.get('/',(req,res)=>res.json({hello:'world'}));
+
+//MiddleWare
+
+app.use(cors());
+app.use(bodyParser.json());
 
 //handling errors 
 app.use(handle.notFound);
